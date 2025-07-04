f024ab5beda4fd09e9cb2ca73d5a95fb42e00915
Juan <juanawbest@gmail.com>
2025-07-04 22:06:33 +0800

    7/4/2025 Done View LaporanGelar

--- app/Filament/Resources/LaporanGelarResource/Pages/ViewLaporanGelar.php
+++ app/Filament/Resources/LaporanGelarResource/Pages/ViewLaporanGelar.php
@@ -2,18 +2,27 @@
 
 namespace App\Filament\Resources\LaporanGelarResource\Pages;
 
+use App\Models\Gelar;
+use Filament\Resources\Pages\Page;
 use App\Filament\Resources\LaporanGelarResource;
-use Filament\Actions;
-use Filament\Resources\Pages\ViewRecord;
 
-class ViewLaporanGelar extends ViewRecord
+class ViewLaporanGelar extends Page
 {
     protected static string $resource = LaporanGelarResource::class;
 
-    protected function getHeaderActions(): array
+    protected static string $view = 'filament.resources.gelar-resource.pages.formulir';
+
+    public $record;
+
+    public function mount($record): void
+    {
+        $this->record = Gelar::with(['mobil.alats', 'detailGelars.alat'])->findOrFail($record);
+    }
+
+    protected function getViewData(): array
     {
         return [
-            Actions\EditAction::make(),
+            'gelar' => $this->record,
         ];
     }
 }
